8c28dc5c11a2ce5069c7bbc8387b92a3ff6aa4d3
Aman Menda <aman.menda@epitech.eu>
2025-01-25 18:43:42 +0100

    feat(providers): add auth provider

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:takali/app_locator.dart';
+import 'package:takali/providers/base.provider.dart';
+import 'package:takali/services/auth_service.dart';
+
+class AuthProvider extends BaseProvider {
+  bool isUserLoggedIn;
+  GlobalKey<NavigatorState> navigatorkey = GlobalKey<NavigatorState>();
+
+  AuthProvider() : isUserLoggedIn = locator<AuthService>().isUserLoggedIn {}
+
+  void onAuthStateChanged() {
+    locator<Logger>().i("user auth state changed");
+    isUserLoggedIn = locator<AuthService>().isUserLoggedIn;
+    notifyListeners();
+  }
+}
