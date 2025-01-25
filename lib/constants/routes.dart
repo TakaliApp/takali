d2337bf3a2f9b710f5a440add928cbaf1988576f
Aman Menda <aman.menda@epitech.eu>
2025-01-25 16:37:39 +0100

    feat(routes): add basic routes

---
+++ lib/constants/routes.dart
@@ -0,0 +1,12 @@
+// all route paths here
+
+class RoutePaths {
+  RoutePaths._(); // Private constructor to prevent external instantiation
+
+  static const String login = '/';
+  static const String verification = '/otp';
+  static const String profile = '/profile';
+  static const String referralSource  = '/referalSource';
+  static const String genderPreference = '/genderPreference';
+  static const String home = '/home';
+}
